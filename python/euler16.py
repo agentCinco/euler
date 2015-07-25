f6ba3d81f6da5de42432341c26e5534a354c7d29
Ty Auvil <ty.auvil@gmail.com>
2015-07-25 17:46:58 -0600

    16

--- python/euler16.py
+++ python/euler16.py
@@ -1,10 +1,17 @@
-#!/usr/bin/python
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # projecteuler.net
 #
 # problem 16
+#
 # 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 #
 # What is the sum of the digits of the number 21000?
 
-print sum([int(x) for x in str(2**1000)])
+n = 2**1000
+x = 0
+
+for i in str(n):
+    x += int(i)
+
+print x
