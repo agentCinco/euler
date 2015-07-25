6d660fa2eecbf351a830614a245c7b22a6a49cdc
Ty Auvil <ty@Tys-MacBook-Pro.local>
2015-07-25 13:59:47 -0600

    housekeeping

--- python/euler7.py
+++ python/euler7.py
@@ -1,29 +1,13 @@
 #! /usr/bin/env python
 
 # projecteuler.net
-# 
+#
 # problem 7
 #
 # What is the 10 001st prime number?
 
-def primes(n): 
-	if n==2: return [2]
-	elif n<2: return []
-	s=range(3,n+1,2)
-	mroot = n ** 0.5
-	half=(n+1)/2-1
-	i=0
-	m=3
-	while m <= mroot:
-		if s[i]:
-			j=(m*m-3)/2
-			s[j]=0
-			while j<half:
-				s[j]=0
-				j+=m
-		i=i+1
-		m=2*i+3
-	return [2]+[x for x in s if x]
+import pyprimes
 
-x = primes(150000)
-print x[10000]
+x = pyprimes.nth_prime(10001)
+
+print x
