a09e54ca10c477fa36034295558b57256956a853
Ty Auvil <ty.auvil@gmail.com>
2015-07-26 00:12:57 -0600

    21

---
+++ python/euler21.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# projecteuler.net
+#
+# problem 21
+#
+
+n = 0
+
+def factors(n):
+    return set(reduce(list.__add__,
+    ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+
+for i in range(220, 10001):
+    x = (sum(factors(i)) - i)
+    if (sum(factors(x)) -x) == i and x != i:
+        n += i
+
+print n
