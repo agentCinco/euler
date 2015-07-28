620374474332624804d0b576f2cee31da031cd89
Ty Auvil <ty.auvil@gmail.com>
2015-07-28 00:22:40 -0600

    25

---
+++ python/euler25.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# projecteuler.net
+#
+# problem 25
+#
+
+def fib(n):
+ a,b = 1,1
+ for i in range(n-1):
+  a,b = b,a+b
+ return a
+
+for i in range(4781, 10000):
+    x = fib(i)
+    if len(str(x)) == 1000:
+        print(i)
+        exit()
