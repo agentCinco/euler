f2536c6fa24a32bfb8b7b69ce556eb9a0915790a
Ty Auvil <ty.auvil@gmail.com>
2015-08-24 07:43:04 -0600

    32

--- python/euler32.py
+++ python/euler32.py
@@ -13,8 +13,6 @@ for i in range(1,100):
     for j in range(10, 10000 // i):
         x = i*j
         z = ''.join(sorted(str(i)+str(j)+str(x)))
-        if len(z) != 9:
-            continue
         if z == nums:
             result.add(x)
 
