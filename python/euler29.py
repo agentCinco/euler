d9a2244bc71f94ce4e2b73afd3e284318200996a
Ty Auvil <ty.auvil@gmail.com>
2015-08-23 18:35:02 -0600

    euler 29

---
+++ python/euler29.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# projecteuler.net
+#
+# problem 29
+#
+
+a=[]
+
+for i in range(2,101):
+    for j in range(2,101):
+        a.append(i**j)
+
+print(len(set(a)))
