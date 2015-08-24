807bbd8619985279213cfba7875c0d7a047ffdb0
Ty Auvil <ty.auvil@gmail.com>
2015-08-23 18:28:40 -0600

    euler 28

--- python/euler27.py
+++ python/euler27.py
@@ -2,21 +2,19 @@
 # -*- coding: utf-8 -*-
 # projecteuler.net
 #
-# problem
+# problem 27
 #
 
 from pyprimesieve import primes
 
 s = 0
 c = (0, 0)
-#optimizing
 p = set(primes(751000))
 
 for a in range(-999, 0, 2):
     for b in range(-a, 1000, 2):
         n = 1
         while (n*(n+a) + b) in p:
-        #while p.issuperset([n*(n+a) + b]):
             n += 1
         if n > s:
             s = n
