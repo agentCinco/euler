b49aeed4d7acf0f2c95b45d3769ea1b911ed0058
Ty Auvil <ty.auvil@gmail.com>
2015-08-23 20:33:29 -0600

    31

---
+++ python/euler31.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# projecteuler.net
+#
+# problem 30
+#
+
+tot = 200
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+
+def change(tot, coins):
+    ways = [0] * (tot + 1)
+    ways[0] = 1
+    for coin in coins:
+        for j in range(coin, tot + 1):
+            ways[j] += ways[j - coin]
+    return ways[tot]
+
+print(change(tot, coins))
