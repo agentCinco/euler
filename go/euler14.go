5c8d4ce5575288daaf93b181ff6423e6849d311f
ty <ty.auvil@gmail.com>
2018-02-03 15:27:02 -0700

    go answer euler14

---
+++ go/euler14.go
@@ -0,0 +1,37 @@
+//
+// projecteuler.net
+//
+// problem 12
+//
+
+package main
+
+import (
+	"fmt"
+)
+
+var hi = 0
+var n = 0
+var r = 1
+var h = 0
+
+func main() {
+	for i := 1; i < 999999; i++ {
+		n = i
+		for n > 2 {
+			if n%2 == 0 {
+				n = n / 2
+				r += 1
+			} else {
+				n = n*3 + 1
+				r += 1
+			}
+			if r > h {
+				h = r
+				hi = i
+			}
+		}
+		r = 0
+	}
+	fmt.Printf("%v\n", hi)
+}
