ccc70eb67da4c340586f395b10bd87ba103de56c
ty <ty.auvil@gmail.com>
2018-02-10 16:02:01 -0700

    improved go answer for 10

--- go/euler10.go
+++ go/euler10.go
@@ -8,15 +8,58 @@ package main
 
 import (
 	"fmt"
-	"math/big"
+	"math"
 )
 
-func main() {
-	answer := 0
-	for i := 1; i < 1999999; i++ {
-		if big.NewInt(int64(i)).ProbablyPrime(10) {
-			answer += i
+func generate_nprimes(N int) []int {
+	var x, y, n int
+	nsqrt := math.Sqrt(float64(N))
+
+	is_prime := make([]bool, N)
+
+	for x = 1; float64(x) <= nsqrt; x++ {
+		for y = 1; float64(y) <= nsqrt; y++ {
+			n = 4*(x*x) + y*y
+			if n <= N && (n%12 == 1 || n%12 == 5) {
+				is_prime[n] = !is_prime[n]
+			}
+			n = 3*(x*x) + y*y
+			if n <= N && n%12 == 7 {
+				is_prime[n] = !is_prime[n]
+			}
+			n = 3*(x*x) - y*y
+			if x > y && n <= N && n%12 == 11 {
+				is_prime[n] = !is_prime[n]
+			}
 		}
+	}
+
+	for n = 5; float64(n) <= nsqrt; n++ {
+		if is_prime[n] {
+			for y = n * n; y < N; y += n * n {
+				is_prime[y] = false
+			}
+		}
+	}
+
+	is_prime[2] = true
+	is_prime[3] = true
+
+	primes := make([]int, 0, 1270606)
+	for x = 0; x < len(is_prime)-1; x++ {
+		if is_prime[x] {
+			primes = append(primes, x)
+		}
+	}
+
+	return primes
+}
+
+func main() {
+	p := generate_nprimes(1999999)
+	answer := 0
+	for _, v := range p {
+		answer += v
 	}
 	fmt.Printf("%v\n", answer)
 }
