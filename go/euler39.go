dc8adfeb9b8ef7a6a17b44284d41afec043678cc
ty <ty.auvil@gmail.com>
2018-02-10 19:48:04 -0700

    cleanup

--- go/euler39.go
+++ go/euler39.go
@@ -1,21 +1,9 @@
-// int pMax = 0, tMax = 0;
-// int m = 0, k = 0;
+//
+// projecteuler.net
+//
+// problem 39
+//
 
-// for (int s = 1; s <= 1000; s++) {
-//     int t = 0;
-//     int mlimit = (int)Math.Sqrt(s / 2);
-//     for (m = 2; m <= mlimit; m++) {
-//         if ((s / 2) % m == 0) { // m found
-//             if (m % 2 == 0) { // ensure that we find an odd number for k
-//                 k = m + 1;
-//             } else {
-//                 k = m + 2;
-//             }
-//             while (k < 2 * m && k <= s / (2 * m)) { if (s / (2 * m) % k == 0 && gcd(k, m) == 1) { t++; } k += 2; } } } if (t > tMax) {
-//         tMax = t;
-//         pMax = s;
-//     }
-// }
 package main
 
 import (
