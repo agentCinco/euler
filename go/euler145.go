7b47596d663f0e1762e6422aaf1d1278e66de35c
ty <ty.auvil@gmail.com>
2018-02-12 22:20:47 -0700

    answer 145 go

--- go/euler145.go
+++ go/euler145.go
@@ -15,12 +15,11 @@ func main() {
 	answer := 0.0
 
 	for i := 2; i < 10; i++ {
-		j := float64(i)
 		if i%2 == 0 {
-			n := math.Floor(j/2 - 1)
+			n := math.Floor(float64(i)/2 - 1)
 			answer += 20 * math.Pow(30, n)
 		} else if i%4 == 3 {
-			n := math.Floor(j / 4)
+			n := math.Floor(float64(i) / 4)
 			answer += 100 * math.Pow(500, n)
 		}
 
